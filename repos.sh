9c6c743e59cff2d8eb507951cd6c515f50a3e215
szymon <szymonso1009@gmail.com>
2021-02-12 15:00:38 +0100

    first commit

---
+++ repos.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/Alfredonator/logger.git
+git clone https://github.com/Alfredonator/grasper.git
+git clone https://github.com/Alfredonator/eDO_moveit.git
+git clone https://github.com/Alfredonator/geometry_representation.git
+git clone https://github.com/Alfredonator/eDO_gazebo.git
+git clone https://github.com/Alfredonator/eDO_description.git
+git clone https://github.com/Alfredonator/eDO_control_v3.git
+git clone https://github.com/Alfredonator/eDO_core_msgs.git
+git clone https://github.com/Alfredonator/camera_node.git
+
